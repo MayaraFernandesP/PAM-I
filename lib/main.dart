41489cd173842f8a8e088cdf8b9a2062454e5b34
MayaraFernandesP <mayara.fernanades2@etec.sp.gov.br>
2024-03-04 20:56:01 -0300

    branch com o código da aula do dia Mon, Mar  4, 2024  8:56:01 PM

---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+//o método main é onde toda aplicação baseada em dart começa
+void main() => runApp(const Aplicacao());
+
+// classe aplicacao, reponsavel por gerencia todo o nosso app
+class Aplicacao extends StatelessWidget {
+  const Aplicacao({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //MaterialApp é responsavel por gerencia  estabelecer padoes
+    //para todas as telas app, nele temos:
+    //configuração de telas
+    //estilos a serem aplicados na tela (via tema)
+    //toda estruturação de tela baseada no Material (material.io)
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+//class que representa a tela do aplicativo com todo seu
+//layout
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Center(
+              child: Text(
+                "Resumo do Pedido",
+                style: textTheme.headlineLarge,
+              ),
+            ),
+            Row(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(right: 10),
+                  child: const Text("1x"),
+                ),
+                const Expanded(child: Text("Banana")),
+                const Text("2,80"),
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(right: 10),
+                  child: const Text("2x"),
+                ),
+                const Expanded(child: Text("Morango")),
+                const Text("8,90"),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
